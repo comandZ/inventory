14bbad894b3d14361588ed0753b4f41ea004f6bd
Patrick Schippers <schippers.patrick@gmail.com>
2023-03-16 20:30:54 -0400

    Adding notes and initial object array to begin inventory CRUD

--- index.js
+++ index.js
@@ -4,10 +4,25 @@ const express = require('express');
 const app = express();
 const PORT = 8080
 
+const inventory = [{
+  name: '', 
+  type: '', 
+  status: '',
+  quantity: ''
+}];
+
 // Create app route (req: request, res: response)
 app.get('/', (req, res) => {
   res.send('Hello Fuker');
 });
+
+// CRUD = DB => Server
+// C: Create  => post
+// R: Read    => get
+// U: Update  => put
+// D: Delete  => delete
+
+// TODO: Make a full '/inventory' route
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);

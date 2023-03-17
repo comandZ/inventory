da63df49396ae638f118480ba60efe0ba606d506
Patrick Schippers <schippers.patrick@gmail.com>
2023-03-16 20:08:27 -0400

    Adding console message to identify server is running

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 const express = require('express');
 // instantiate server app
 const app = express();
+const PORT = 8080
 
 // Create app route (req: request, res: response)
 app.get('/', (req, res) => {
   res.send('Hello Fuker');
 });
 
-app.listen(8080);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});

4cd5431f0a932b669cc8701c45898887625a3b4e
Patrick Schippers <schippers.patrick@gmail.com>
2023-03-16 20:03:22 -0400

    Adding base express application and initiation from run dev

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello Fucker");
+// node webapp framework
+const express = require('express');
+// instantiate server app
+const app = express();
+
+// Create app route (req: request, res: response)
+app.get('/', (req, res) => {
+  res.send('Hello Fuker');
+});
+
+app.listen(8080);

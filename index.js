1e7c2b2f63d2620af15376ca41a91c8c2f880065
Patrick Schippers <schippers.patrick@gmail.com>
2023-03-20 19:15:04 -0400

    Adding first GET calls to internal inventory

--- index.js
+++ index.js
@@ -4,12 +4,26 @@ const express = require('express');
 const app = express();
 const PORT = 8080
 
-const inventory = [{
-  name: '', 
-  type: '', 
-  status: '',
-  quantity: ''
-}];
+const INVENTORY = [
+  {
+  name: 'chips', 
+  type: 'food', 
+  status: 'home',
+  quantity: 3
+},
+{
+  name: 'cookies', 
+  type: 'food', 
+  status: 'couch',
+  quantity: 6
+},
+{
+  name: 'movies', 
+  type: 'entertainment', 
+  status: 'living',
+  quantity: 300
+},
+];
 
 // Create app route (req: request, res: response)
 app.get('/', (req, res) => {
@@ -23,6 +37,16 @@ app.get('/', (req, res) => {
 // D: Delete  => delete
 
 // TODO: Make a full '/inventory' route
+
+// GET for inventory
+app.get('/inventory', (req, res) => {
+  res.send(INVENTORY);
+});
+
+// Thoughy Experiment
+app.get('/inventory/last-item', (req, res) => {
+  res.send(INVENTORY[INVENTORY.length - 1]);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
